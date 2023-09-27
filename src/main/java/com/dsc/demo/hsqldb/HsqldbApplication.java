f97f7709b0784fbcf5d3060c7bf2ce7efe22c008
dsc-cquevedo <camilo.quevedo@exlservice.com>
2023-09-27 10:07:01 -0500

    first commit

---
+++ src/main/java/com/dsc/demo/hsqldb/HsqldbApplication.java
@@ -0,0 +1,13 @@
+package com.dsc.demo.hsqldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsqldbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsqldbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsc/demo/hsqldb/HsqldbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsc.demo.hsqldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsqldbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
